3670b5fc9dfd232e8f5b1937f90df2d409643d3c
paramesh-md <parameshwaran.pasupathy@rutgers.edu>
2023-12-07 03:40:26 -0500

    Added regression test

--- tests/test_mesh_file.py
+++ tests/test_mesh_file.py
@@ -35,5 +35,29 @@ class TestMeshFile(unittest.TestCase):
         self.assertEqual(u.shape, (grid, 3))
         # Add more assertions here
 
+    def test_regression(self):
+        num_points = [100, 10, 10]
+        time_steps = 4000
+        bounds = [0, 1, 0, 0.1, 0, 0.1]
+        nbounds= 3
+
+        length = 1.0
+        width = 0.1
+        thick = 0.1
+
+        dx, dy, dz, delta, area, volume = mf.mat_parameters(length, width, thick, num_points)
+        grid = mf.total_nodes(num_points, nbounds)
+        numfam, pointfam = mf.number_family(grid)
+
+        u, alflag = mf.create_grid(grid, num_points, dx, width, thick, length)
+
+        # Compare u with expected result
+        expected_u = np.load('expected_u.npy')
+        np.testing.assert_array_almost_equal(u, expected_u, decimal=4)
+
+        # Compare alflag with expected result
+        expected_alflag = np.load('expected_alflag.npy')
+        np.testing.assert_array_almost_equal(alflag, expected_alflag, decimal=4)
+
 if __name__ == '__main__':
     unittest.main()
