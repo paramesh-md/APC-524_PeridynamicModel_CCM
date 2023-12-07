ec83e4a34f028563a7f1d3b481203541377ebe02
paramesh-md <parameshwaran.pasupathy@rutgers.edu>
2023-12-07 03:21:11 -0500

    Add unit test for mesh_file

---
+++ tests/test_mesh_file.py
@@ -0,0 +1,39 @@
+import unittest
+import numpy as np
+import mesh_file as mf
+
+class TestMeshFile(unittest.TestCase):
+    def test_mat_parameters(self):
+        length = 1.0
+        width = 0.1
+        thick = 0.1
+        num_points = [100, 10, 10]
+        dx, dy, dz, delta, area, volume = mf.mat_parameters(length, width, thick, num_points)
+        self.assertEqual(dx, 0.01)
+        # Add more assertions here
+
+    def test_total_nodes(self):
+        num_points = [103, 10, 10]
+        nbounds = 3
+        grid = mf.total_nodes(num_points, nbounds)
+        self.assertEqual(grid, 10303)
+        # Add more assertions here
+
+    def test_number_family(self):
+        grid = 10300
+        numfam, pointfam = mf.number_family(grid)
+        # Add assertions here
+
+    def test_create_grid(self):
+        num_points = [100, 10, 10]
+        length = 1.0
+        width = 0.1
+        thick = 0.1
+        dx = 0.01
+        grid = 10300
+        u, alflag = mf.create_grid(grid, num_points, dx, width, thick, length)
+        self.assertEqual(u.shape, (grid, 3))
+        # Add more assertions here
+
+if __name__ == '__main__':
+    unittest.main()
